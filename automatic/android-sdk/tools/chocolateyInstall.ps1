13293b11cf5b699d5ff676843530627beca0997e
Justin James <digitaldrummerj@gmail.com>
2016-05-23 20:27:21 -0700

    fixed the urls for the nuspec.  updated owners.  added env path update

--- automatic/android-sdk/tools/chocolateyInstall.ps1
+++ automatic/android-sdk/tools/chocolateyInstall.ps1
@@ -1,10 +1,5 @@
-﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  $downUrl = '{{DownloadUrl}}'
-  $md5hash = '29ca8cb8f0bc8db627fa2adc2139a3cc'
-  Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' $downUrl -validExitCodes @(0)
+﻿$downUrl = '{{DownloadUrl}}'
+Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' $downUrl -validExitCodes @(0)
 
-  #Write-ChocolateySuccess 'android-sdk'
-#} catch {
-  #Write-ChocolateyFailure 'android-sdk' "$($_.Exception.Message)"
-  #throw 
-#}
+Install-ChocolateyPath "${Env:LOCALAPPDATA}\Android\android-sdk\tools" 'Machine'
+Install-ChocolateyPath "${Env:LOCALAPPDATA}\Android\android-sdk\platform-tools" 'Machine'
