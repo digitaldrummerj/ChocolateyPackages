82ae91a7a1f51ff1dd57c03637f8713175a0aa53
Justin James <digitaldrummerj@gmail.com>
2016-06-17 18:24:45 -0700

    started work on android studio automatic update pkg

---
+++ automatic/AndroidStudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,46 @@
+﻿
+# Common Functions and Config
+. (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Common.ps1')
+
+if (Test-Path ($installDir)) {
+    $uninstallExe = GetUninstallFile
+    
+    $params = @{
+        PackageName = $package;
+        FileType = "exe";
+        SilentArgs = "/S";
+        File = $uninstallExe;
+    }
+
+    Uninstall-ChocolateyPackage @params
+}
+
+# Url = "https://dl.google.com/dl/android/studio/install/$majorVersion/android-studio-ide-$buildVersion-windows.exe"
+# Checksum = "$checksum";
+# CheckSumType = "sha1";
+
+$params = @{
+	PackageName = $package;
+	Url = '{{DownloadUrl}}'
+	InstallerType = "exe";
+	SilentArgs = "/S";	
+	validExitCodes = @(0,1223);
+}
+
+Install-ChocolateyPackage @params
+	
+$customArgs = $env:chocolateyPackageParameters
+$settings = GetArguments $customArgs
+$studioExe = GetStudioExe
+
+if ($settings.addtodesktop -eq "true")
+{
+	Write-Host "AddToDesktop Value: " $settings.addtodesktop
+	Install-ChocolateyShortcut $studioExe
+}
+
+if ($settings.pinnedtotaskbar -eq "true")
+{
+	Write-Host "PinnedToTaskbar Value: " $settings.pinnedtotaskbar
+	Install-ChocolateyPinnedTaskBarItem $studioExe
+}
