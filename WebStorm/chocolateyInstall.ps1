7d5cb0951aff9f91f694f1774752e61a0731f2a7
Justin James <digitaldrummerj@gmail.com>
2016-06-27 11:29:02 -0700

    updated to 2016.1.3

--- WebStorm/chocolateyInstall.ps1
+++ WebStorm/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $installerType = 'EXE' 
-$url = "http://download.jetbrains.com/webstorm/WebStorm-2016.1.2.exe" 
+$url = "http://download.jetbrains.com/webstorm/WebStorm-11.0.4.exe" 
 $url64 = $url 
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
