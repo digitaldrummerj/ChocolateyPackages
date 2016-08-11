f932679728a0c729929b999a23382df16d8eadf2
Justin James <digitaldrummerj@gmail.com>
2016-08-10 20:26:13 -0700

    upgraded to 2016.2.1

--- WebStorm/chocolateyInstall.ps1
+++ WebStorm/chocolateyInstall.ps1
@@ -1,8 +1,9 @@
 $installerType = 'EXE' 
-$url = "https://download.jetbrains.com/webstorm/WebStorm-2016.2.exe" 
+$url = "https://download.jetbrains.com/webstorm/WebStorm-2016.2.1.exe" 
 $url64 = $url 
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
+$checksumType = 'sha256';
+$checksum = 'ff8e2babefff47b23beb7960d83f289b5725f92e30f12d6b628524ae95cee544';
 
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -Checksum64 "$checksum" -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum "$checksum"  
