4e3c8a1939dddfa56be941d8130c88d13ce402d6
Alex Shafer <shafer.alex@gmail.com>
2016-09-18 23:35:27 -0400

    Correct Path Variables
    The install was not installing to `${Env:LOCALAPPDATA}\Android\android-sdk` but was instead installing to `${Env:ProgramFiles(x86}\Android\android-sdk\tools` or`${Env:ProgramFiles}\Android\android-sdk\platform-tools`.  This corrects the path location.

--- automatic/android-sdk/tools/chocolateyInstall.ps1
+++ automatic/android-sdk/tools/chocolateyInstall.ps1
@@ -1,7 +1,13 @@
 ﻿$downUrl = '{{DownloadUrl}}'
 $checksum = '{checksum}'
 $checksumType = 'sha256'
-Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' $downUrl -validExitCodes @(0)   -Checksum "$checksum" -ChecksumType "$checksumType"
+Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' $downUrl -validExitCodes @(0)  -Checksum "$checksum" -ChecksumType "$checksumType"
 
-Install-ChocolateyPath "${Env:LOCALAPPDATA}\Android\android-sdk\tools" 'Machine'
-Install-ChocolateyPath "${Env:LOCALAPPDATA}\Android\android-sdk\platform-tools" 'Machine'
+if ("${Env:ProgramFiles(x86)}")
+{
+    Install-ChocolateyPath "${Env:ProgramFiles(x86}\Android\android-sdk\tools" 'Machine'
+}
+else
+{
+    Install-ChocolateyPath "${Env:ProgramFiles}\Android\android-sdk\platform-tools" 'Machine'
+}
