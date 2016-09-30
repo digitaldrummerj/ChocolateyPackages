d334836ff223ad8ab210f9f2b7d101800f5070f2
Alex Shafer <shafer.alex@gmail.com>
2016-09-30 10:04:24 -0400

    Add `Install for All Users` installer.

---
+++ automatic/android-sdk.install/tools/chocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+﻿
+UnInstall-ChocolateyPath "${Env:LOCALAPPDATA}\Android\android-sdk\tools" 'Machine'
+UnInstall-ChocolateyPath "${Env:LOCALAPPDATA}\Android\android-sdk\platform-tools" 'Machine'
+
+$packageName = '{{PackageName}}'
+$unpath = "${Env:LOCALAPPDATA}\Android\android-sdk\uninstall.exe"
+Uninstall-ChocolateyPackage $packageName 'exe' '/S' $unpath
+  
