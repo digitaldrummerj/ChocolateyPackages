97e0c4786fc39fb52ed7129e8f55d10bdde6d384
Alex Shafer <shafer.alex@gmail.com>
2016-09-30 09:51:07 -0400

    Revert "Correct Path Variables"
    This reverts commit 4e3c8a1939dddfa56be941d8130c88d13ce402d6.

--- automatic/android-sdk/tools/chocolateyInstall.ps1
+++ automatic/android-sdk/tools/chocolateyInstall.ps1
@@ -1,13 +1,7 @@
 ﻿$downUrl = '{{DownloadUrl}}'
 $checksum = '{checksum}'
 $checksumType = 'sha256'
-Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' $downUrl -validExitCodes @(0)  -Checksum "$checksum" -ChecksumType "$checksumType"
+Install-ChocolateyPackage '{{PackageName}}' 'exe' '/S' $downUrl -validExitCodes @(0)   -Checksum "$checksum" -ChecksumType "$checksumType"
 
-if ("${Env:ProgramFiles(x86)}")
-{
-    Install-ChocolateyPath "${Env:ProgramFiles(x86}\Android\android-sdk\tools" 'Machine'
-}
-else
-{
-    Install-ChocolateyPath "${Env:ProgramFiles}\Android\android-sdk\platform-tools" 'Machine'
-}
+Install-ChocolateyPath "${Env:LOCALAPPDATA}\Android\android-sdk\tools" 'Machine'
+Install-ChocolateyPath "${Env:LOCALAPPDATA}\Android\android-sdk\platform-tools" 'Machine'
