661ce5b05144ba0b370af0e87ba75ffa38b3d0bf
zed <dev@zedcn.com>
2017-03-03 12:04:34 +0800

    Update WebStorm to 2016.3.3

--- WebStorm/chocolateyInstall.ps1
+++ WebStorm/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $installerType = 'EXE' 
-$url = "https://download.jetbrains.com/webstorm/WebStorm-2016.3.exe" 
+$url = "https://download.jetbrains.com/webstorm/WebStorm-2016.3.3.exe" 
 $url64 = $url 
 $silentArgs = '/S' 
 $validExitCodes = @(0) 
 $checksumType = 'sha256';
-$checksum = '0a8ef9562c826148d6ea6d230e35b680e99a7867f3cc181803f5e5ebc718c29b';
+$checksum = '48b866ac9e2d8860b1c60b4a6da77e8b725467f0c49ec9c9a765429718ea582e';
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes -Checksum64 "$checksum" -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum "$checksum"  
